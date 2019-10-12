49b05368966da6a999f29f9249638461ff902918
Bonkook Koo <djmimi@nate.com>
2019-10-13 02:01:46 +0900

    Add sample components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Banner>
+      This is a banner!
+    </Banner>
+    <Input />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Banner from './components/Banner.vue'
+import Input from './components/Input.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Banner,
+    Input
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Input.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input class="test" v-model="text" @click="onClick" />
+  </div>
+</template>
+<script>
+export default {
+  name: 'Input',
+  data() {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    onClick() {
+      console.log( 'onClick' );
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .test {
+    border: 1px solid red;
+  }
+</style>
